d8b323051c03a627a175d7673c9ffbf51e3dfd2c
valengaro <valengaro@gmail.com>
2024-06-07 13:30:20 +0200

    mejorando

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import plotly.express as px
 import streamlit as st
 df=px.data.gapminder()
-fig=px.scatter(data_frame=df,
+with st.sidebar:
+    st.write('Selecciona el Año')
+    year=st.selectbox('Year',options=[2007,2003,2001])
+
+mask=df['year']==year
+df_filter=df[mask]
+fig=px.scatter(data_frame=df_filter,
            x='gdpPercap',
            y='lifeExp',
            animation_frame='year',
@@ -12,4 +18,5 @@ fig=px.scatter(data_frame=df,
            log_x=True
            )
 st.plotly_chart(fig)
+
 # haciendo pruebas
